caf9047ca2d5a654175c5a4e65672ccf381bfa91
kbrat32c1 <kevin.bratland@gmail.com>
2024-10-25 19:07:03 -0400

    Update config.py

--- config.py
+++ config.py
@@ -4,12 +4,13 @@ class Config:
     # Get the base directory of the current file
     basedir = os.path.abspath(os.path.dirname(__file__))
     
-    # Path to the SQLite database
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'instance', 'wrestling.db')
+    # Read database URL from environment variable
+    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL', 'sqlite:///' + os.path.join(os.path.abspath(os.path.dirname(__file__)), 'instance', 'wrestling.db'))
     
     # Disable FS overhead for SQLAlchemy
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
     # Other useful configurations
-    SECRET_KEY = 'your_secret_key_here'  # Replace with a secure key
+    SECRET_KEY = 'a53b32f4238ac0b42700093518c19674a620544082a56ba9'  # Replace with a secure key
     SEND_FILE_MAX_AGE_DEFAULT = 0
+
